3d72797471a35bd6b54f502b8def964cb9867007
Arjun Bhatnagar <arjxbh@gmail.com>
2022-01-01 22:08:28 -0500

    created endpoint that returns available bluetooth addresses

--- python/main.py
+++ python/main.py
@@ -2,7 +2,17 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+import asyncio
+from bleak import discover
+
+import json
+
 app = FastAPI()
+
+class BluetoothDevice:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
 
 
 @app.get("/")
@@ -13,3 +23,14 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/discover")
+async def read_root():
+    devices = await discover()
+    devs = [];
+    print(devices)
+    for d in devices:
+        bd = BluetoothDevice(d.name, d.address)
+        devs.append(bd)
+    return devs
