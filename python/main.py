2a78ae070084559d991ce621097d504258cccd7d
Arjun Bhatnagar <arjxbh@gmail.com>
2022-01-01 23:47:22 -0500

    pull speed and cadence data from the trainer

--- python/main.py
+++ python/main.py
@@ -4,6 +4,9 @@ from fastapi import FastAPI
 
 import asyncio
 from bleak import discover
+from bleak import BleakClient
+
+from pycycling.cycling_speed_cadence_service import CyclingSpeedCadenceService
 
 import json
 
@@ -15,22 +18,68 @@ class BluetoothDevice:
         self.address = address
 
 
+class SensorResponse:
+    def __init__(self, cwr, lwet, cr, et):
+        self.cumulative_wheel_revs = cwr
+        self.last_wheel_event_time = lwet
+        self.cumulative_crank_revs = cr
+        self.last_crank_event_time = et
+
+
+class BicycleSensorResponse:
+    lastResponse = None
+    def pageHandler(self, data):
+        print('were in the page handler')
+        print(data)
+        self.lastResponse = data
+
+
+async def readSpeedCadence(address):
+    async with BleakClient(address) as client:
+        responseHandler = BicycleSensorResponse()
+        await client.is_connected()
+        trainer = CyclingSpeedCadenceService(client)
+        trainer.set_csc_measurement_handler(responseHandler.pageHandler)
+        await trainer.enable_csc_measurement_notifications()
+        await asyncio.sleep(5.0)
+        await trainer.disable_csc_measurement_notifications()
+        return responseHandler.lastResponse
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
 
 
 @app.get("/discover")
 async def read_root():
     devices = await discover()
-    devs = [];
+    foundDevices = [];
     print(devices)
     for d in devices:
-        bd = BluetoothDevice(d.name, d.address)
-        devs.append(bd)
-    return devs
+        bTDev = BluetoothDevice(d.name, d.address)
+        foundDevices.append(bTDev)
+    return foundDevices
+
+
+@app.get("/sensor/speedAndCadence/{bluetoothAddress}")
+async def read_item(bluetoothAddress: str):
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(readSpeedCadence(bluetoothAddress))
+    try:
+        bluetoothResponse = await readSpeedCadence(bluetoothAddress)
+        print(bluetoothResponse)
+        apiResponse = SensorResponse(bluetoothResponse[0], bluetoothResponse[1], bluetoothResponse[2], bluetoothResponse[3])
+        print(apiResponse)
+        return apiResponse
+    except Exception as e:
+        ## TODO: fix me, should return 4xx or 5xx status code
+        #return 'failed to get speed and cadence data'
+        return e
+
+# TO RUN: uvicorn main:app --reload
